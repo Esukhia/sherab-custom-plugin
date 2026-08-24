a348357127198366663ba1c27e95601d1cba69e0
tenkalden <tenkalden007@gmail.com>
2026-08-24 17:08:19 +0530

    remove unused imports from course_partnerships urls.py (django.conf.settings, re_path, include)

--- course_partnerships/urls.py
+++ course_partnerships/urls.py
@@ -2,9 +2,7 @@
 Defines the URL routes for this app.
 """
 
-from django.conf import settings
-from django.urls import path, re_path
-from django.conf.urls import include
+from django.urls import path
 
 from .views import *
 
